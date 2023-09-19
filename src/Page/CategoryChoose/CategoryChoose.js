14d0f97cab43ef91c851dc66ecda45abbe70cce9
sanjeet kumar <skc0845@gmail.com>
2023-09-19 23:08:17 +0530

    added side view category page

--- src/Page/CategoryChoose/CategoryChoose.js
+++ src/Page/CategoryChoose/CategoryChoose.js
@@ -1,10 +1,23 @@
 import "./CategoryChoose.css"
+import cross from "../../assets/cross.png"
+import error from "../../assets/error.png"
 
 function CategoryChoose() {
     return (
-        <div>
-            Hello chhose category now
-            <h1>jksdjklsk</h1>
+        <div className="category-container">
+            <div className="side-view">
+                <h3>Super app</h3>
+                <p>Choose your entertainment category</p>
+                <div className="selected-category">
+                    <div>Romance<img src={cross} alt="close_icon" /></div>
+                    <div>Thriller<img src={cross} alt="close_icon" /></div>
+                    <div>Action<img src={cross} alt="close_icon" /></div>
+                </div>
+                <div className="error">
+                    <img src={error} alt="alert_icon" />
+                    Minimum 3 category required
+                </div>
+            </div>
         </div>
     )
 }
